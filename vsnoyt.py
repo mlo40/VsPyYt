031cbfadd5db02d5ec8a5a5e603927ebeb9ed2e3
emlo40 <emils.gailis@gmail.com>
2021-08-23 12:25:19 +0300

    added requirements.txt and cleared vsnotyt.py for user modification

--- vsnoyt.py
+++ vsnoyt.py
@@ -25,12 +25,13 @@ async def main():
             print(authtoken)
             print('Saving authtoken for Future Use...')
             with open('token.json', "w") as json_file:
-                jsonfilecon = [{"streamid": ""},{"authenticationkey": authtoken}]
+                jsonfilecon = [{"authenticationkey": authtoken}]
                 json_file.write(json.dumps(jsonfilecon))
                 #json.dump(authtoken, json_file)
         await authen(websocket,authtoken)
         while True:
-            await mdmv(websocket,0.2,False,0.1,0.1,300,-22.5)
-            await TintArtM(websocket,255,150,0,255,True,"","","","","")
+            ###########
+            #code here#
+            ###########
             time.sleep(0.1)
 asyncio.get_event_loop().run_until_complete(main())
