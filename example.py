0000635145d8dba819372a4bbe9c1ffec18355e5
emlo40 <emils.gailis1@gmail.com>
2021-08-24 14:16:28 +0300

    Update example.py

--- example.py
+++ example.py
@@ -53,6 +53,8 @@ async def main():
         #############################
         await authen(websocket,authtoken)
         while True:
-            #code here
+            ###############
+            #  code here  #
+            ###############
             time.sleep(0.1)
 asyncio.get_event_loop().run_until_complete(main())
